d180561d0e13b460c44318fa96cee2a49211469c
Ismael <ismasaleme12@gmail.com>
2026-09-10 20:51:47 -0300

    Agrega Carpeta USERS

---
+++ users/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from .models import User 
+
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField (
+        label='Username',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tu usuario'})
+    )
+
+    password = forms.CharField (
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tu contraseña'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
